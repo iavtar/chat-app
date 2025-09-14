c10c399a5fa9a09caaa308c2f9c9d25213c1fdd2
iavtar <indra.avatarsingh@gmail.com>
2025-09-14 15:29:33 +0530

    added code for ui and be

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("This is signup route");
+};
+
+export const login = (req, res) => {
+  res.send("This is login route");
+};
+
+export const logout = (req, res) => {
+  res.send("This is logout route");
+};
